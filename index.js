e3572de5df06b039deb3853c950a7c53f001ccad
Jaap Barnhoorn <jaap@aanzee.nl>
2018-10-04 15:21:57 +0200

    add transports option

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var io = require('socket.io')(server, { origins: '*'});
 
 app.use(cors());
 io.origins(['http://localhost:3333', 'https://hhb-adventkalender.netlify.com']);
+io.set('transports', ['websocket', 'polling']);
 
 app.set('port', (process.env.PORT || 5000));
 server.listen(app.get('port'));
